0232a25dfe22f0f84febe85637ba31fd52bb75b4
Perazzojoao <perazzojoao@gmail.com>
2024-10-14 23:54:52 -0300

    feat: adding security filter for authentication

--- src/main/java/com/security/auth_api/repositories/UserRepository.java
+++ src/main/java/com/security/auth_api/repositories/UserRepository.java
@@ -5,7 +5,10 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.security.auth_api.domain.user.User;
+import java.util.Optional;
+
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+  Optional<User> findByEmail(String email);
 
 }
